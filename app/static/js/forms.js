5dd587f62611aa758f13359aa04c7d867e93d3f8
Joël Kuiper <me@joelkuiper.eu>
2025-08-17 19:54:14 +0200

    Loading states for profile related buttons
    - Renamed the file to forms.js since that's all it does, but might change my
    mind later.

--- app/static/js/forms.js
+++ app/static/js/forms.js
@@ -7,9 +7,13 @@ document.addEventListener("DOMContentLoaded", () => {
       if (!btn || btn.dataset.spinning === "1") return;
       const loadingText = btn.dataset.loading || "Loading";
       startButtonSpinner(btn, loadingText);
+
+      // Fallback to stop the spinner if navigation fails
+      setTimeout(() => {
+        if (btn.dataset.spinning === "1") stopButtonSpinner(btn);
+      }, 10000);
     });
   });
 });
 
-export { stopButtonSpinner };
 
